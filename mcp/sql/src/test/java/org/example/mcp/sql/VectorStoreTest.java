dd5905286cc2782b5dec8b5bbc93d5bde621a0a2
lobashin <lobashin@users.noreply.github.com>
2026-01-25 10:14:02 +0700

    add test vector store

--- mcp/sql/src/test/java/org/example/mcp/sql/VectorStoreTest.java
+++ mcp/sql/src/test/java/org/example/mcp/sql/VectorStoreTest.java
@@ -1,6 +1,7 @@
 package org.example.mcp.generator.sql;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 public class VectorStoreTest {
@@ -16,19 +20,45 @@ public class VectorStoreTest {
     @Autowired
     VectorStore vectorStore;
 
+    @BeforeEach
+    void beforeEach() {
+        List<String> allIds = Objects.requireNonNull(vectorStore.similaritySearch(SearchRequest.builder()
+                        .build()))
+                .stream()
+                .map(Document::getId)
+                .collect(Collectors.toList());
+        vectorStore.delete(allIds);
+    }
+
     @Test
-    void similaritySearch_shouldFoundOnlyOneMostSimilarSql(){
+    void similaritySearch_shouldFoundOnlyOneMostSimilarSql() {
+        //given
+        var documents = List.of(
+                Document.builder()
+                        .id(UUID.randomUUID().toString())
+                        .metadata("firstMetadataKey", "firstMetadataValue")
+                        .score(0.0)
+                        .text("firstText")
+                        .build());
+        vectorStore.add(documents);
 
-
+        //when
         List<Document> results = vectorStore.similaritySearch(
-                SearchRequest.builder().query("test")
+                SearchRequest.builder().query("Покажи sql для поиска сотрудника с детьми и с идетификатором 4 или 3")
                         .topK(10)
                         .similarityThreshold(0.7).build());
 
+        //then
         Assertions.assertNotNull(results);
         Assertions.assertEquals(1, results.size());
-        Assertions.assertEquals("select * from my", results.getFirst().getText());
-
+        Assertions.assertEquals("""
+                        SELECT employes.name FROM datamart.employee AS employes
+                        LEFT JOIN datamart.children AS childrens
+                        ON employes.id = childrens.employee_id
+                        WHERE employes.id in (4, 3)
+                        ORDER BY employes.id ASC
+                        """,
+                results.getFirst().getText());
 
     }
 }
